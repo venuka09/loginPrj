f8107fb7cc28dda077639835f6cdc75521d21523
venuka <vv356@njit.edu>
2018-03-17 18:08:43 -0400

    Added Unit test cases and feature test cases.

---
+++ tests/Feature/RegisterTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class RegisterTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testRegisterPage()
+    {
+        $response = $this->get('/register');
+
+        $response->assertStatus(200);
+    }
+    public function testLoginPage()
+    {
+        $response = $this->get('/login');
+
+        $response->assertStatus(200);
+    }
+}
